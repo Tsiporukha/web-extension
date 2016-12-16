65576e7cffd2e6808b79a126d4d8d261b0dfdda5
surov <brainsurov@gmail.com>
2016-12-16 16:05:12 +0200

    Player redesign
    Hide tabs navigation

--- src/scripts/containers/Player.js
+++ src/scripts/containers/Player.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
 import { connect } from 'react-redux';
-
+import Slider from 'react-toolbox/lib/slider';
 import {play, pause, setVolume, setProgress, playNextSong, playPrevSong} from '../actions/PlayerActions'
+import {duration} from '../lib/duration';
+import styles from '../../assets/styles/player.scss';
 
 const mapStateToProps = (state, ownProps) => {
   return state.player ?
@@ -20,14 +22,15 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     next: (currentSong, playlistPath) => { dispatch(playNextSong(currentSong, playlistPath)) },
     prev: (currentSong, playlistPath) => { dispatch(playPrevSong(currentSong, playlistPath)) },
     pause: () => { dispatch(pause()) },
-    setVolume: (e) => { dispatch(setVolume(parseFloat(e.target.value))) },
+//    setVolume: (e) => { dispatch(setVolume(parseFloat(e.target.value))) },
+    setVolume: (value) => dispatch(setVolume(value)),
     setProgress: (progress) => { dispatch(setProgress(progress)) }
   }
 }
 
 class Player extends Component {
 
-  state = {seeking: false};
+  state = {seeking: false, duration: 0};
 
   onSeekMouseDown = e => { this.setState({ seeking: true }) };
 
@@ -51,11 +54,37 @@ class Player extends Component {
 
   render () {
     return (
-      <div style={{position: 'fixed', bottom: '0px', backgroundColor: 'cadetblue',
-        visibility: this.props.playingSong.data_url ? 'visible' : 'hidden' }}>
+      <div className={styles["bottom-player"]} style={{visibility: this.props.playingSong.data_url ? 'visible' : 'hidden' }}>
+        <div className={styles["cover"]}>
+      		<img src={this.props.playingSong.artwork_url} className={styles["cover"]}/>
+      	</div>
+      	<div className={styles["song-info"]} >
+      		<h5>{this.props.playingSong.artist}</h5>
+      		<h6>{this.props.playingSong.title}</h6>
+      	</div>
+        <i onClick={e => {this.props.prev(this.props.playingSong, this.props.playingSong.playlist)}}
+          className="material-icons">&#xE045;</i>
+        {this.props.playing ?
+      	  <i className={`material-icons ${styles['mi-bpause']}`} onClick={this.props.pause}>&#xE034;</i> :
+	        <i className={`material-icons ${styles['mi-bpause']}`} onClick={this.props.play}>&#xE037;</i>
+        }
+      	<i onClick={e => {this.props.next(this.props.playingSong, this.props.playingSong.playlist)}}
+          className="material-icons">&#xE044;</i>
+
+        <i className="material-icons">&#xE04D;</i>
+      	<div className={styles["player-volume"]}> <Slider min={0} max={1} value={this.props.volume} onChange={this.props.setVolume} /> </div>
+        {duration(this.state.duration * this.props.played)}
+      	<div className={styles["song-progress"]} >
+          <input type='range' step='any'  min={0} max={1} value={this.props.played}
+             onMouseUp={this.onSeekMouseUp}
+             onMouseDown={this.onSeekMouseDown}
+             onChange={e => { this.props.setProgress({played: parseFloat(e.target.value)}) }}/>
+        </div>
+        {duration(this.state.duration *(1 - this.props.played))}
+
         <ReactPlayer
           ref={player => { this.player = player }}
-          className='react-player'
+          className={styles.reactPlayer}
           width={240}
           height={135}
           url={this.props.playingSong.data_url}
@@ -64,23 +93,8 @@ class Player extends Component {
           onProgress={this.onProgress}
           onEnded={() => { this.props.next(this.props.playingSong, this.props.playingSong.playlist) }}
           onReady={this.syncPlayer}
-          onDuration={duration => { console.log(duration) }}
+          onDuration={duration => { this.setState({ duration }) }}
         />
-        {this.props.playing ?
-          <button className={'btn btn-warning'} onClick={this.props.pause}>pause</button> :
-          <button className={'btn btn-success'} onClick={this.props.play}>play</button>
-        }
-        <button onClick={e => {this.props.prev(this.props.playingSong, this.props.playingSong.playlist)}}
-          className={'btn btn-info'}>prev</button>
-        <button onClick={e => {this.props.next(this.props.playingSong, this.props.playingSong.playlist)}}
-          className={'btn btn-info'}>next</button>
-        volume
-        <input type='range' min={0} max={1} step='any' value={this.props.volume} onChange={this.props.setVolume} />
-        progress
-        <input type='range' min={0} max={1} step='any' value={this.props.played}
-          onMouseDown={this.onSeekMouseDown}
-          onMouseUp={this.onSeekMouseUp}
-          onChange={e => { this.props.setProgress({played: parseFloat(e.target.value)}) }}/>
       </div>
     )
   }
